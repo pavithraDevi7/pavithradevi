6fb299e2898b22b37d338cbcf1ec36fa958692c4
pavithradevi <pavithradeviganesh38@gmail.com>
2025-05-20 10:07:38 +0530

    commit2

---
+++ script.js
@@ -0,0 +1,24 @@
+var a=10;
+var b=20;
+c=a+b;d=a-b;e=a*b;j=a/b;w=a%b;
+z=a<b;x=a!=b;q=b>a; 
+
+console.log(a,d,e,c,j,w,z,x,q)
+function add()
+{
+    var a=10;
+    var b=20;
+    console.log(a+b)
+
+
+}
+add()
+function add(a,b){
+console.log(a+b)
+}
+add(10,10)
+add(20,20)
+add(10,20)
+
+
+
